7bd0a0f4792753ffdc4b4ae9a8276fa547b68b52
Tiago Queiroz <tiago.queiroz@elastic.co>
2022-04-07 10:30:20 +0200

    update go.mod to provide github.com/elastic/beats/v8

--- libbeat/processors/script/javascript/session_test.go
+++ libbeat/processors/script/javascript/session_test.go
@@ -23,9 +23,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/elastic/beats/v7/libbeat/beat"
-	"github.com/elastic/beats/v7/libbeat/common"
-	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v8/libbeat/beat"
+	"github.com/elastic/beats/v8/libbeat/common"
+	"github.com/elastic/beats/v8/libbeat/logp"
 
 	"github.com/stretchr/testify/assert"
 )
