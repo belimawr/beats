belimawr/beats

commit b23261ed82abd814b9e95fbe050add47eca83f7a

[Heartbeat] Fix origin, Cleanup stdfields (backport #31877) (#31915)
* [Heartbeat] Fix origin, Cleanup stdfields (#31877)

Fixes #31870 and refactors the wrapper code to be cleaner and more consistent.

Prior to this PR we would only set the monitor.origin field if the monitor.origin config setting was set on non-browser (e.g. HTTP) monitors. The reason the original patch only worked on lightweight monitors was that there has historically been a separate codepath for adding simple core metadata fields to lightweight monitors than browser based monitors.

This PR removes that codepath, funneling the writing of most browser monitor.* fields through addMonitorMeta in wrappers.go. This means that we'll have more consistent behavior between browser and regular monitors going forward.

This entailed some untangling of some funky code to get working, apologies for the larger diff, but it leaves the codebase more consistent than we found it.

One weird wrinkle is the monitor.project field, which we don't currently use, but customers might. This contains the literal id and name from the monitors YAML when running zip_url or local monitors. This does keep the 8.3 change moving this from suite.{id,name} to monitor.project.{id,name} but does it in a cleaner way, labeling as many relevant variables, tests, and functions as legacy. One day we can remove this when / if we remove support for zip_url and local browser sources.

We should probably do this before the beta is over.

This PR also tries to more consistently refer to instances of stdfields.StandardFields as sFields when used as a var.

(cherry picked from commit 96c586c37c58cad67be9ffb9ad51a144180ad18c)

* Make linter happy

Co-authored-by: Andrew Cholakian <andrewvc@elastic.co>

heartbeat/monitors/stdfields/stdfields.go
@@ -39,7 +39,11 @@ type StdMonitorFields struct {
 	Service           ServiceFields      `config:"service"`
 	Origin            string             `config:"origin"`
 	LegacyServiceName string             `config:"service_name"`
-	Enabled           bool               `config:"enabled"`
+	// Used by zip_url and local monitors
+	// kibana originating monitors only run one journey at a time
+	// and just use the `fields` syntax / manually set monitor IDs
+	IsLegacyBrowserSource bool
+	Enabled               bool `config:"enabled"`
 }
 
 func ConfigToStdMonitorFields(config *conf.C) (StdMonitorFields, error) {

heartbeat/monitors/wrappers/wrappers.go
@@ -69,38 +69,60 @@ func WrapBrowser(js []jobs.Job, stdMonFields stdfields.StdMonitorFields) []jobs.
 		js,
 		addMonitorTimespan(stdMonFields),
 		addServiceName(stdMonFields),
+		addMonitorMeta(stdMonFields, false),
 		addMonitorStatus(true),
 	)
 }
 
 // addMonitorMeta adds the id, name, and type fields to the monitor.
-func addMonitorMeta(sf stdfields.StdMonitorFields, isMulti bool) jobs.JobWrapper {
+func addMonitorMeta(sFields stdfields.StdMonitorFields, hashURLIntoID bool) jobs.JobWrapper {
 	return func(job jobs.Job) jobs.Job {
 		return func(event *beat.Event) ([]jobs.Job, error) {
 			cont, err := job(event)
 
-			id := sf.ID
-			name := sf.Name
+			id := sFields.ID
+			name := sFields.Name
 			// If multiple jobs are listed for this monitor, we can't have a single ID, so we hash the
 			// unique URLs to create unique suffixes for the monitor.
-			if isMulti {
+			if hashURLIntoID {
 				url, err := event.GetValue("url.full")
 				if err != nil {
 					logp.Error(fmt.Errorf("mandatory url.full key missing: %w", err))
 					url = "n/a"
 				}
 				urlHash, _ := hashstructure.Hash(url, nil)
-				id = fmt.Sprintf("%s-%x", sf.ID, urlHash)
+				id = fmt.Sprintf("%s-%x", sFields.ID, urlHash)
 			}
 
 			fields := mapstr.M{
-				"id":   id,
-				"name": name,
-				"type": sf.Type,
+				"type": sFields.Type,
 			}
 
-			if sf.Origin != "" {
-				fields["origin"] = sf.Origin
+			if !sFields.IsLegacyBrowserSource {
+				fields["id"] = id
+				fields["name"] = name
+			} else {
+				// Id and name differs for local
+				// - We use the monitor id and name for inline journeys ignoring the
+				//   autogenerated `inline`journey id and name.
+				// - Monitor id/name is concatenated with the journey id/name for
+				// 	 project monitors
+				journeyId, err := event.GetValue("monitor.id")
+				if err == nil {
+					fields["id"] = fmt.Sprintf("%s-%s", sFields.ID, journeyId)
+				}
+				journeyName, err := event.GetValue("monitor.name")
+				if err == nil {
+					fields["name"] = fmt.Sprintf("%s - %s", sFields.Name, journeyName)
+				}
+				fields["project"] = mapstr.M{
+					"id":   sFields.ID,
+					"name": sFields.Name,
+				}
+			}
+
+			if sFields.Origin != "" {
+				fields["origin"] = sFields.Origin
 			}
 
 			eventext.MergeEventFields(event, mapstr.M{"monitor": fields})

x-pack/heartbeat/monitors/browser/project.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/elastic/beats/v7/heartbeat/monitors/jobs"
 	"github.com/elastic/beats/v7/heartbeat/monitors/plugin"
+	"github.com/elastic/beats/v7/heartbeat/monitors/stdfields"
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/x-pack/heartbeat/monitors/browser/synthexec"
 	"github.com/elastic/elastic-agent-libs/config"
@@ -54,13 +55,6 @@ func (p *Project) Workdir() string {
 	return p.projectCfg.Source.Active().Workdir()
 }
 
-func (p *Project) InlineSource() (string, bool) {
-	if p.projectCfg.Source.Inline != nil {
-		return p.projectCfg.Source.Inline.Script, true
-	}
-	return "", false
-}
-
 func (p *Project) Params() map[string]interface{} {
 	return p.projectCfg.Params
 }
@@ -69,14 +63,18 @@ func (p *Project) FilterJourneys() synthexec.FilterJourneyConfig {
 	return p.projectCfg.FilterJourneys
 }
 
-func (p *Project) Fields() synthexec.StdProjectFields {
-	_, isInline := p.InlineSource()
-	return synthexec.StdProjectFields{
-		Name:     p.projectCfg.Name,
-		Id:       p.projectCfg.Id,
-		IsInline: isInline,
-		Type:     "browser",
+func (p *Project) Fields() stdfields.StdMonitorFields {
+	sFields := stdfields.StdMonitorFields{
+		Name: p.projectCfg.Name,
+		ID:   p.projectCfg.Id,
+		Type: "browser",
 	}
+
+	if p.projectCfg.Source.Local != nil || p.projectCfg.Source.ZipUrl != nil {
+		sFields.IsLegacyBrowserSource = true
+	}
+
+	return sFields
 }
 
 func (p *Project) Close() error {
@@ -130,7 +128,9 @@ func (p *Project) extraArgs() []string {
 
 func (p *Project) jobs() []jobs.Job {
 	var j jobs.Job
-	if src, ok := p.InlineSource(); ok {
+	isScript := p.projectCfg.Source.Inline != nil
+	if isScript {
+		src := p.projectCfg.Source.Inline.Script
 		j = synthexec.InlineJourneyJob(context.TODO(), src, p.Params(), p.Fields(), p.extraArgs()...)
 	} else {
 		j = func(event *beat.Event) ([]jobs.Job, error) {

x-pack/heartbeat/monitors/browser/synthexec/enrich.go
@@ -16,21 +16,23 @@ import (
 
 	"github.com/elastic/beats/v7/heartbeat/eventext"
 	"github.com/elastic/beats/v7/heartbeat/monitors/logger"
+	"github.com/elastic/beats/v7/heartbeat/monitors/stdfields"
 	"github.com/elastic/beats/v7/libbeat/beat"
 )
 
-type enricher func(event *beat.Event, se *SynthEvent, fields StdProjectFields) error
+type enricher func(event *beat.Event, se *SynthEvent) error
 
 type streamEnricher struct {
-	je *journeyEnricher
+	je      *journeyEnricher
+	sFields stdfields.StdMonitorFields
 }
 
-func (e *streamEnricher) enrich(event *beat.Event, se *SynthEvent, fields StdProjectFields) error {
-	if e.je == nil || (se != nil && se.Type == JourneyStart) {
-		e.je = newJourneyEnricher()
+func (senr *streamEnricher) enrich(event *beat.Event, se *SynthEvent) error {
+	if senr.je == nil || (se != nil && se.Type == JourneyStart) {
+		senr.je = newJourneyEnricher(senr)
 	}
 
-	return e.je.enrich(event, se, fields)
+	return senr.je.enrich(event, se)
 }
 
 // journeyEnricher holds state across received SynthEvents retaining fields
@@ -44,14 +46,16 @@ type journeyEnricher struct {
 	stepCount       int
 	// The first URL we visit is the URL for this journey, which is set on the summary event.
 	// We store the URL fields here for use on the summary event.
-	urlFields mapstr.M
-	start     time.Time
-	end       time.Time
+	urlFields      mapstr.M
+	start          time.Time
+	end            time.Time
+	streamEnricher *streamEnricher
 }
 
-func newJourneyEnricher() *journeyEnricher {
+func newJourneyEnricher(senr *streamEnricher) *journeyEnricher {
 	return &journeyEnricher{
-		checkGroup: makeUuid(),
+		checkGroup:     makeUuid(),
+		streamEnricher: senr,
 	}
 }
 
@@ -63,7 +67,7 @@ func makeUuid() string {
 	return u.String()
 }
 
-func (je *journeyEnricher) enrich(event *beat.Event, se *SynthEvent, fields StdProjectFields) error {
+func (je *journeyEnricher) enrich(event *beat.Event, se *SynthEvent) error {
 	if se == nil {
 		return nil
 	}
@@ -84,40 +88,10 @@ func (je *journeyEnricher) enrich(event *beat.Event, se *SynthEvent, fields StdP
 		event.Timestamp = time.Now()
 	}
 
-	// Id and name differs for inline and project monitors
-	// - We use the monitor id and name for inline journeys ignoring the
-	//   autogenerated `inline`journey id and name.
-	// - Monitor id/name is concatenated with the journey id/name for
-	// 	 project monitors
-	id := fields.Id
-	name := fields.Name
-	if !fields.IsInline && je.journey != nil {
-		id = fmt.Sprintf("%s-%s", id, je.journey.Id)
-		name = fmt.Sprintf("%s - %s", name, je.journey.Name)
-	}
 	eventext.MergeEventFields(event, mapstr.M{
-		"event": mapstr.M{
-			"type": se.Type,
-		},
-		"monitor": mapstr.M{
-			"check_group": je.checkGroup,
-			"id":          id,
-			"name":        name,
-			"type":        fields.Type,
-		},
+		"event.type": se.Type,
 	})
 
-	// Write project level fields for project monitors
-	if !fields.IsInline {
-		eventext.MergeEventFields(event, mapstr.M{
-			"monitor": mapstr.M{
-				"project": mapstr.M{
-					"id":   fields.Id,
-					"name": fields.Name,
-				},
-			},
-		})
-	}
 	return je.enrichSynthEvent(event, se)
 }
 

commit 5a634d513361ee10d869b3ef5988ccda9f7f312e

[Heartbeat] Fix missing check group field for browser monitors (#31997)
The check group field is totally missing from heartbeat data, fully breaking the Uptime UI

Fixes a regression from https://github.com/elastic/beats/pull/31915/files#diff-b6c88c98d3a0073b9edf475ef0c1e713ba39dccff126a1b99ac107cafea863edL98 where the monitor.check_group field would no longer be present in browser monitor jobs.

Also fixed issue where zip_url / local suites would not set id / name fields to the names of the individual journeys.

This moves more logic out of the synthexec enrich code and into the wrapper code, where setting fields can be centralized. As a general principle, centralizing the creation of fields is a good thing.

This PR doesn't aim to necessarily de-complexify code. I think we could make things cleaner / simpler, but we don't want to hold up the release train at the moment.

heartbeat/eventext/eventext.go
@@ -37,11 +37,15 @@ const EventCancelledMetaKey = "__hb_evt_cancel__"
 // CancelEvent marks the event as cancelled. Downstream consumers of it should not emit nor output this event.
 func CancelEvent(event *beat.Event) {
 	if event != nil {
-		if event.Meta == nil {
-			event.Meta = mapstr.M{}
-		}
-		_, _ = event.Meta.Put(EventCancelledMetaKey, true)
+		SetMeta(event, EventCancelledMetaKey, true)
 	}
+}
+
+func SetMeta(event *beat.Event, k string, v interface{}) {
+	if event.Meta == nil {
+		event.Meta = mapstr.M{}
+	}
+	_, _ = event.Meta.Put(k, v)
 }
 
 // IsEventCancelled checks for the marker left by CancelEvent.

heartbeat/monitors/stdfields/stdfields.go
@@ -22,7 +22,7 @@ import (
 	"time"
 
 	"github.com/elastic/beats/v7/heartbeat/scheduler/schedule"
-	conf "github.com/elastic/elastic-agent-libs/config"
+	"github.com/elastic/elastic-agent-libs/config"
 )
 
 type ServiceFields struct {
@@ -44,22 +44,32 @@ type StdMonitorFields struct {
 	// and just use the `fields` syntax / manually set monitor IDs
 	IsLegacyBrowserSource bool
 	Enabled               bool `config:"enabled"`
+	// TODO: Delete this once browser / local monitors are removed
+	Source struct {
+		ZipUrl *config.C `config:"zip_url"`
+		Local  *config.C `config:"local"`
+	} `config:"source"`
 }
 
-func ConfigToStdMonitorFields(config *conf.C) (StdMonitorFields, error) {
-	mpi := StdMonitorFields{Enabled: true}
+func ConfigToStdMonitorFields(conf *config.C) (StdMonitorFields, error) {
+	sFields := StdMonitorFields{Enabled: true}
 
-	if err := config.Unpack(&mpi); err != nil {
-		return mpi, fmt.Errorf("error unpacking monitor plugin config: %w", err)
+	if err := conf.Unpack(&sFields); err != nil {
+		return sFields, fmt.Errorf("error unpacking monitor plugin config: %w", err)
 	}
 
 	// Use `service_name` if `service.name` is unspecified
 	// `service_name` was only document in the 7.10.0 release.
-	if mpi.LegacyServiceName != "" {
-		if mpi.Service.Name == "" {
-			mpi.Service.Name = mpi.LegacyServiceName
+	if sFields.LegacyServiceName != "" {
+		if sFields.Service.Name == "" {
+			sFields.Service.Name = sFields.LegacyServiceName
 		}
 	}
 
-	return mpi, nil
+	// TODO: Delete this once browser / local monitors are removed
+	if sFields.Source.Local != nil || sFields.Source.ZipUrl != nil {
+		sFields.IsLegacyBrowserSource = true
+	}
+
+	return sFields, nil
 }

heartbeat/monitors/wrappers/wrappers.go
@@ -71,6 +71,7 @@ func WrapBrowser(js []jobs.Job, stdMonFields stdfields.StdMonitorFields) []jobs.
 		addServiceName(stdMonFields),
 		addMonitorMeta(stdMonFields, false),
 		addMonitorStatus(true),
+		logJourneySummaries,
 	)
 }
 
@@ -98,7 +99,7 @@ func addMonitorMeta(sFields stdfields.StdMonitorFields, hashURLIntoID bool) jobs
 				"type": sFields.Type,
 			}
 
-			if !sFields.IsLegacyBrowserSource {
+			if !sFields.IsLegacyBrowserSource || sFields.Type != "browser" {
 				fields["id"] = id
 				fields["name"] = name
 			} else {
@@ -226,6 +227,27 @@ func addMonitorDuration(job jobs.Job) jobs.Job {
 		}
 
 		return cont, err
+	}
+}
+
+const META_STEP_COUNT = "__HEARTBEAT_STEP_COUNT__"
+
+// logJourneySummaries emits a metric for the service when summary events are complete.
+// Only applies to browser journeys.
+func logJourneySummaries(job jobs.Job) jobs.Job {
+	return func(event *beat.Event) ([]jobs.Job, error) {
+		conts, err := job(event)
+
+		summary, _ := event.GetValue("summary")
+		if summary != nil {
+			sc, _ := event.Meta.GetValue(META_STEP_COUNT)
+			var scInt int
+			// If we don't have it we have zero steps
+			scInt, _ = sc.(int)
+			logger.LogRun(event, &scInt)
+		}
+
+		return conts, err
 	}
 }
 

x-pack/heartbeat/monitors/browser/project.go
@@ -63,17 +63,13 @@ func (p *Project) FilterJourneys() synthexec.FilterJourneyConfig {
 	return p.projectCfg.FilterJourneys
 }
 
-func (p *Project) Fields() stdfields.StdMonitorFields {
-	sFields := stdfields.StdMonitorFields{
-		Name: p.projectCfg.Name,
-		ID:   p.projectCfg.Id,
-		Type: "browser",
+func (p *Project) StdFields() stdfields.StdMonitorFields {
+	sFields, err := stdfields.ConfigToStdMonitorFields(p.rawCfg)
+	// Should be impossible since outer monitor.go should run this same code elsewhere
+	// TODO: Just pass stdfields in to remove second deserialize
+	if err != nil {
+		logp.L().Warnf("Could not deserialize monitor fields for browser, this should never happen: %s", err)
 	}
-
-	if p.projectCfg.Source.Local != nil || p.projectCfg.Source.ZipUrl != nil {
-		sFields.IsLegacyBrowserSource = true
-	}
-
 	return sFields
 }
 
@@ -131,14 +127,14 @@ func (p *Project) jobs() []jobs.Job {
 	isScript := p.projectCfg.Source.Inline != nil
 	if isScript {
 		src := p.projectCfg.Source.Inline.Script
-		j = synthexec.InlineJourneyJob(context.TODO(), src, p.Params(), p.Fields(), p.extraArgs()...)
+		j = synthexec.InlineJourneyJob(context.TODO(), src, p.Params(), p.StdFields(), p.extraArgs()...)
 	} else {
 		j = func(event *beat.Event) ([]jobs.Job, error) {
 			err := p.Fetch()
 			if err != nil {
 				return nil, fmt.Errorf("could not fetch for project job: %w", err)
 			}
-			sj, err := synthexec.ProjectJob(context.TODO(), p.Workdir(), p.Params(), p.FilterJourneys(), p.Fields(), p.extraArgs()...)
+			sj, err := synthexec.ProjectJob(context.TODO(), p.Workdir(), p.Params(), p.FilterJourneys(), p.StdFields(), p.extraArgs()...)
 			if err != nil {
 				return nil, err
 			}

x-pack/heartbeat/monitors/browser/synthexec/enrich.go
@@ -15,8 +15,8 @@ import (
 	"github.com/gofrs/uuid"
 
 	"github.com/elastic/beats/v7/heartbeat/eventext"
-	"github.com/elastic/beats/v7/heartbeat/monitors/logger"
 	"github.com/elastic/beats/v7/heartbeat/monitors/stdfields"
+	"github.com/elastic/beats/v7/heartbeat/monitors/wrappers"
 	"github.com/elastic/beats/v7/libbeat/beat"
 )
 
@@ -105,6 +105,18 @@ func (je *journeyEnricher) enrichSynthEvent(event *beat.Event, se *SynthEvent) e
 		}
 	}
 
+	// Needed for the edge case where a console log is emitted after one journey ends
+	// but before another begins.
+	if je.journey != nil {
+		eventext.MergeEventFields(event, mapstr.M{
+			"monitor": mapstr.M{
+				"check_group": je.checkGroup,
+				"id":          je.journey.ID,
+				"name":        je.journey.Name,
+			},
+		})
+	}
+
 	switch se.Type {
 	case CmdStatus:
 		// If a command failed _after_ the journey was complete, as it happens
@@ -132,7 +144,7 @@ func (je *journeyEnricher) enrichSynthEvent(event *beat.Event, se *SynthEvent) e
 		event.SetID(se.Id)
 		// This is only relevant for screenshots, which have a specific ID
 		// In that case we always want to issue an update op
-		_, _ = event.Meta.Put(events.FieldMetaOpType, events.OpTypeCreate)
+		eventext.SetMeta(event, events.FieldMetaOpType, events.OpTypeCreate)
 	}
 
 	eventext.MergeEventFields(event, se.ToMap())
@@ -185,10 +197,7 @@ func (je *journeyEnricher) createSummary(event *beat.Event) error {
 		},
 	})
 
-	_, err := event.GetValue("monitor.id")
-	if err == nil {
-		logger.LogRun(event, &je.stepCount)
-	}
+	eventext.SetMeta(event, wrappers.META_STEP_COUNT, je.stepCount)
 
 	if je.journeyComplete {
 		return je.firstError

heartbeat/monitors/wrappers/wrappers_test.go
@@ -466,6 +466,7 @@ type BrowserMonitor struct {
 	id         string
 	name       string
 	checkGroup string
+	durationMs int64
 	// Used for testing legacy zip_url and local monitors
 	// where the top-level id/name are used to populate monitor.project
 	legacyProjectId   string
@@ -528,6 +529,7 @@ var projectMonitorValues = BrowserMonitor{
 	id:         "project-journey_1",
 	name:       "project-Journey 1",
 	checkGroup: "journey-1-check-group",
+	durationMs: time.Second.Microseconds(),
 }
 
 // Used for testing legacy zip_url / local monitorss
@@ -543,6 +545,7 @@ func makeProjectBrowserJob(t *testing.T, u string, summary bool, projectErr erro
 	parsed, err := url.Parse(u)
 	require.NoError(t, err)
 	return func(event *beat.Event) (i []jobs.Job, e error) {
+		eventext.SetMeta(event, META_STEP_COUNT, 2)
 		eventext.MergeEventFields(event, mapstr.M{
 			"url": URLFields(parsed),
 			"monitor": mapstr.M{
@@ -550,6 +553,7 @@ func makeProjectBrowserJob(t *testing.T, u string, summary bool, projectErr erro
 				"id":          bm.id,
 				"name":        bm.name,
 				"check_group": bm.checkGroup,
+				"duration":    mapstr.M{"us": bm.durationMs},
 			},
 		})
 		if summary {
@@ -567,6 +571,21 @@ func makeProjectBrowserJob(t *testing.T, u string, summary bool, projectErr erro
 	}
 }
 
+var browserLogValidator = func(monId string, expectedDurationUs int64, stepCount int) func(t *testing.T, events []*beat.Event, observed []observer.LoggedEntry) {
+	return func(t *testing.T, events []*beat.Event, observed []observer.LoggedEntry) {
+		require.Len(t, observed, 1)
+		require.Equal(t, "Monitor finished", observed[0].Message)
+
+		durationMs := expectedDurationUs / 1000
+		expectedMonitor := logger.NewMonitorRunInfo(monId, "browser", durationMs)
+		expectedMonitor.Steps = &stepCount
+		require.ElementsMatch(t, []zap.Field{
+			logp.Any("event", map[string]string{"action": logger.ActionMonitorRun}),
+			logp.Any("monitor", &expectedMonitor),
+		}, observed[0].Context)
+	}
+}
+
 func TestProjectBrowserJob(t *testing.T) {
 	sFields := testBrowserMonFields
 	sFields.ID = projectMonitorValues.id
@@ -580,6 +599,7 @@ func TestProjectBrowserJob(t *testing.T) {
 			"type":        "browser",
 			"id":          projectMonitorValues.id,
 			"name":        projectMonitorValues.name,
+			"duration":    mapstr.M{"us": time.Second.Microseconds()},
 			"origin":      "my-origin",
 			"check_group": projectMonitorValues.checkGroup,
 			"timespan": mapstr.M{
@@ -618,7 +638,7 @@ func TestProjectBrowserJob(t *testing.T) {
 					}),
 				))},
 		nil,
-		nil,
+		browserLogValidator(projectMonitorValues.id, time.Second.Microseconds(), 2),
 	})
 	testCommonWrap(t, testDef{
 		"with down summary",
@@ -639,7 +659,7 @@ func TestProjectBrowserJob(t *testing.T) {
 					}),
 				))},
 		nil,
-		nil,
+		browserLogValidator(projectMonitorValues.id, time.Second.Microseconds(), 2),
 	})
 
 	legacySFields := testBrowserMonFields
@@ -649,9 +669,10 @@ func TestProjectBrowserJob(t *testing.T) {
 
 	expectedLegacyMonFields := lookslike.MustCompile(map[string]interface{}{
 		"monitor": map[string]interface{}{
-			"type": "browser",
-			"id":   fmt.Sprintf("%s-%s", legacyProjectMonitorValues.legacyProjectId, legacyProjectMonitorValues.id),
-			"name": fmt.Sprintf("%s - %s", legacyProjectMonitorValues.legacyProjectName, legacyProjectMonitorValues.name),
+			"type":     "browser",
+			"id":       fmt.Sprintf("%s-%s", legacyProjectMonitorValues.legacyProjectId, legacyProjectMonitorValues.id),
+			"name":     fmt.Sprintf("%s - %s", legacyProjectMonitorValues.legacyProjectName, legacyProjectMonitorValues.name),
+			"duration": mapstr.M{"us": int64(0)},
 			"project": mapstr.M{
 				"id":   legacyProjectMonitorValues.legacyProjectId,
 				"name": legacyProjectMonitorValues.legacyProjectName,

x-pack/heartbeat/monitors/browser/synthexec/enrich_test.go
@@ -11,19 +11,15 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	"go.uber.org/zap/zaptest/observer"
 
-	"github.com/elastic/beats/v7/heartbeat/monitors/logger"
 	"github.com/elastic/beats/v7/heartbeat/monitors/stdfields"
 	"github.com/elastic/beats/v7/heartbeat/monitors/wrappers"
 	"github.com/elastic/beats/v7/libbeat/beat"
 	"github.com/elastic/beats/v7/libbeat/beat/events"
 	"github.com/elastic/beats/v7/libbeat/processors/add_data_stream"
-	"github.com/elastic/elastic-agent-libs/logp"
 	"github.com/elastic/elastic-agent-libs/mapstr"
 	"github.com/elastic/go-lookslike"
+	"github.com/elastic/go-lookslike/isdef"
 	"github.com/elastic/go-lookslike/testslike"
 	"github.com/elastic/go-lookslike/validator"
 )
@@ -105,6 +101,7 @@ func TestJourneyEnricher(t *testing.T) {
 				"synthetics.type":     "heartbeat/summary",
 				"url":                 wrappers.URLFields(u),
 				"monitor.duration.us": int64(journeyEnd.Timestamp().Sub(journeyStart.Timestamp()) / time.Microsecond),
+				"monitor.check_group": isdef.IsString,
 			}))
 		}
 		return lookslike.Compose(v...)
@@ -468,31 +465,20 @@ func TestSummaryWithoutJourneyEnd(t *testing.T) {
 func TestCreateSummaryEvent(t *testing.T) {
 	baseTime := time.Now()
 
-	defaultLogValidator := func(stepCount int) func(t *testing.T, summary mapstr.M, observed []observer.LoggedEntry) {
-		return func(t *testing.T, summary mapstr.M, observed []observer.LoggedEntry) {
-			require.Len(t, observed, 1)
-			require.Equal(t, "Monitor finished", observed[0].Message)
-
-			durationMs := baseTime.Add(10 * time.Microsecond).Sub(baseTime).Milliseconds()
-			expectedMonitor := logger.NewMonitorRunInfo("my-monitor", "browser", durationMs)
-			expectedMonitor.Steps = &stepCount
-			require.ElementsMatch(t, []zap.Field{
-				logp.Any("event", map[string]string{"action": logger.ActionMonitorRun}),
-				logp.Any("monitor", &expectedMonitor),
-			}, observed[0].Context)
-		}
+	testJourney := Journey{
+		ID:   "my-monitor",
+		Name: "My Monitor",
 	}
 
 	tests := []struct {
-		name         string
-		je           *journeyEnricher
-		expected     mapstr.M
-		wantErr      bool
-		logValidator func(t *testing.T, summary mapstr.M, observed []observer.LoggedEntry)
+		name     string
+		je       *journeyEnricher
+		expected mapstr.M
+		wantErr  bool
 	}{{
 		name: "completed without errors",
 		je: &journeyEnricher{
-			journey:         &Journey{},
+			journey:         &testJourney,
 			start:           baseTime,
 			end:             baseTime.Add(10 * time.Microsecond),
 			journeyComplete: true,
@@ -505,12 +491,11 @@ func TestCreateSummaryEvent(t *testing.T) {
 				"up":   1,
 			},
 		},
-		wantErr:      false,
-		logValidator: defaultLogValidator(3),
+		wantErr: false,
 	}, {
 		name: "completed with error",
 		je: &journeyEnricher{
-			journey:         &Journey{},
+			journey:         &testJourney,
 			start:           baseTime,
 			end:             baseTime.Add(10 * time.Microsecond),
 			journeyComplete: true,
@@ -524,12 +509,11 @@ func TestCreateSummaryEvent(t *testing.T) {
 				"up":   0,
 			},
 		},
-		wantErr:      true,
-		logValidator: defaultLogValidator(0),
+		wantErr: true,
 	}, {
 		name: "started, but exited without running steps",
 		je: &journeyEnricher{
-			journey:         &Journey{},
+			journey:         &testJourney,
 			start:           baseTime,
 			end:             baseTime.Add(10 * time.Microsecond),
 			stepCount:       0,
@@ -542,12 +526,11 @@ func TestCreateSummaryEvent(t *testing.T) {
 				"up":   1,
 			},
 		},
-		wantErr:      true,
-		logValidator: defaultLogValidator(0),
+		wantErr: true,
 	}, {
 		name: "syntax error - exited without starting",
 		je: &journeyEnricher{
-			journey:         &Journey{},
+			journey:         &testJourney,
 			end:             time.Now().Add(10 * time.Microsecond),
 			journeyComplete: false,
 			errorCount:      1,
@@ -558,21 +541,11 @@ func TestCreateSummaryEvent(t *testing.T) {
 				"up":   0,
 			},
 		},
-		logValidator: func(t *testing.T, summary mapstr.M, observed []observer.LoggedEntry) {
-			// We don't log run data without duration
-			require.Len(t, observed, 1)
-			require.Equal(t, "Error gathering information to log event", observed[0].Message)
-		},
 		wantErr: true,
 	}}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			core, observed := observer.New(zapcore.InfoLevel)
-			logger.SetLogger(logp.NewLogger("t", zap.WrapCore(func(in zapcore.Core) zapcore.Core {
-				return zapcore.NewTee(in, core)
-			})))
-
 			monitorField := mapstr.M{"id": "my-monitor", "type": "browser"}
 
 			e := &beat.Event{
@@ -591,13 +564,9 @@ func TestCreateSummaryEvent(t *testing.T) {
 				"url":                mapstr.M{},
 				"event.type":         "heartbeat/summary",
 				"synthetics.type":    "heartbeat/summary",
-				"synthetics.journey": Journey{},
+				"synthetics.journey": testJourney,
 			}, true)
 			testslike.Test(t, lookslike.Strict(lookslike.MustCompile(tt.expected)), e.Fields)
-
-			if tt.logValidator != nil {
-				tt.logValidator(t, tt.expected, observed.All())
-			}
 		})
 	}
 }